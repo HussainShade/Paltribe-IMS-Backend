d15ec72b7c600c2247353357466acf0a2eef6845
HussainShade <hussain2k47@gmail.com>
2026-01-21 12:15:58 +0530

    Before integration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import dns from 'dns';
 import app from './app';
 import { Config } from './config';
 import { connectDB, disconnectDB } from './config/database';
+
+// Override DNS servers to bypass ISP issues with MongoDB SRV records
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const startServer = async () => {
   await connectDB();
